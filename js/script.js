b7d0b1268bf394a41082edb91b33648d999ea650
emryswarlock <ardha275@gmail.com>
2019-06-29 21:54:04 +0700

    add some functionality to our to-do list

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@
 var input = document.getElementById("input");
 var button = document.getElementById("button");
 var ul = document.querySelector("ul");
-var listItem = document.getElementsByTagName("li");
-var item = document.querySelectorAll("li");
+
 
 
 function inputLength() {
@@ -12,8 +11,23 @@ function inputLength() {
 }
 
 function createList() {
+
+    var label = document.createElement("label");
+    label.innerHTML = input.value;
+    label.onclick = doneFunction;
+
+    var chcklst = document.createElement("button");
+    chcklst.innerHTML = '✔';
+    chcklst.onclick = doneFunction;
+
+    var delb = document.createElement("button");
+    delb.innerHTML = "❌";
+    delb.onclick = removeParentList;
+
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
+    li.appendChild(label);
+    li.appendChild(chcklst);
+    li.appendChild(delb);
     ul.appendChild(li);
     input.value = "";
 }
@@ -39,7 +53,32 @@ button.addEventListener("click", click);
 input.addEventListener("keypress",pressEnter);
 
 
-item.onclick = function(e){
-    e.target.classList.toggle("done");
+// if user clicked on list item so it will correct
+var labelList = document.querySelectorAll("li label");
+// if checklist button click so the list item also will be correction
+var checklistButton  = document.querySelectorAll("li button.checklist ");
+
+for (let i = 0; i < labelList.length; i++) { // it is for a couple first list item
+    const element = labelList[i];
+    element.addEventListener("click",doneFunction);
+    const check = checklistButton[i];
+    check.addEventListener("click",doneFunction);
 }
+
+function doneFunction(event){
+    console.log(event.target.parentElement.querySelector("label").classList.toggle("done")); // add class "done" to label.
+}
+
+
+// if delte button clicked, the list item will be removing
+var deleteButton = document.querySelectorAll("li button.delete");
+for (let ii = 0; ii < deleteButton.length; ii++) {
+    const element = deleteButton[ii];
+    element.addEventListener("click",removeParentList); // add a click triger
+}
+
+function removeParentList(event) {
+    console.log(event.target.parentElement.remove()); // remove list item
+}
+
 
